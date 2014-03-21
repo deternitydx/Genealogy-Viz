00e4d276efceef188a8b9f570752d24bf7f6f2d9
Robbie Hott <jh2jf@virginia.edu>
2014-03-21 17:28:59 -0400

    Added some new views to the data in the api

---
+++ prototype/api/multimarriages.php
@@ -0,0 +1,62 @@
+<?php
+
+//header('Content-type: application/json');
+
+$id = 14;
+if (isset($_GET["id"]))
+	$id = $_GET["id"];
+	
+
+$db = pg_connect("host=nauvoo.iath.virginia.edu dbname=nauvoo_data user=nauvoo password=p7qNpqygYU");
+
+$result = pg_query($db, "SELECT 
+	  \"Marriage\".\"HusbandID\", 
+	    \"Person\".\"Surname\", 
+	      \"Person\".\"GivenName\", 
+	        \"Person\".\"BirthDateSearchable\", 
+		  \"Person\".\"DeathDateSearchable\",
+		    count(*) as Wives
+		    FROM 
+		      public.\"Marriage\", 
+		        public.\"Person\"
+			WHERE 
+			  \"Marriage\".\"HusbandID\" = \"Person\".\"ID\"
+			  GROUP BY
+			    \"Marriage\".\"HusbandID\", 
+			      \"Person\".\"Surname\", 
+			        \"Person\".\"GivenName\", 
+				  \"Person\".\"BirthDateSearchable\", 
+				    \"Person\".\"DeathDateSearchable\"
+				    ORDER BY Wives desc;");
+
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+
+$arr = pg_fetch_all($result);
+
+echo "<table border='1'>";
+echo "<tr><td>ID</td><td>Surname</td><td>Given Name</td><td>Birth Date</td><td>Death Date</td><td>Number of Wives</td></tr>";
+$json = array();
+foreach ($arr as $mar) {
+	$resa = array();
+	foreach ($mar as $k=>$v) {
+		//array_push($resa,"\"$k\": \"$v\"");
+		if ($k == "HusbandID")
+			array_push($resa, "<a href=\"http://ford.cs.virginia.edu/nauvoo/chord.html?id=$v\">$v</a>");
+		else
+			array_push($resa, "$v");
+	}
+	
+	
+	array_push($json, "<tr><td>" . implode("</td><td>", $resa) . "</td></tr>");
+
+
+}
+	echo implode("", $json);
+
+echo "</table>";
+
+?>
+
