7ffd541c53dd88004d323847323fa8ac1df470cd
Robbie Hott <jh2jf@virginia.edu>
2014-03-26 13:39:55 -0400

    Added other metrics.  Can see marriages before 1845 where the wife died before the second marriage (widower marriages).

---
+++ prototype/api/people.php
@@ -0,0 +1,38 @@
+<?php
+
+//header('Content-type: application/json');
+
+$id = 14;
+if (isset($_GET["id"]))
+	$id = $_GET["id"];
+	
+
+$db = pg_connect("host=nauvoo.iath.virginia.edu dbname=nauvoo_new user=nauvoo password=p7qNpqygYU");
+
+$result = pg_query($db, "SELECT * FROM public.\"Person\" ORDER BY \"ID\" asc");
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+
+$arr = pg_fetch_all($result);
+echo "Results: " . count($arr);
+echo "<table border='1'>";
+$json = array();
+foreach ($arr as $mar) {
+	$resa = array();
+	foreach ($mar as $k=>$v) {
+		//array_push($resa,"\"$k\": \"$v\"");
+		array_push($resa, "$v");
+	}
+	
+	
+	array_push($json, "<tr><td>" . implode("</td><td>", $resa) . "</td></tr>");
+
+
+}
+	echo implode("", $json);
+
+echo "</table>";
+
+?>
