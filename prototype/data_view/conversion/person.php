5925c7ad4f864d85904568a2209010e1252605c0
Robbie Hott <jh2jf@virginia.edu>
2014-06-19 16:50:47 -0400

    working version of person import.

--- prototype/data_view/conversion/person.php
+++ prototype/data_view/conversion/person.php
@@ -1,34 +1,77 @@
 <?php
 
-$db = pg_connect("host=nauvoo.iath.virginia.edu dbname=nauvoo_new user=nauvoo password=p7qNpqygYU");
+$db_from = pg_connect("host=nauvoo.iath.virginia.edu dbname=nauvoo_new user=nauvoo password=p7qNpqygYU");
+$db_to = pg_connect("host=nauvoo.iath.virginia.edu dbname=nauvoo_data user=nauvoo password=p7qNpqygYU");
 
 
 echo "Grabbing all the Person records.\n";
 
-$result = pg_query($db, "SELECT * FROM public.\"Person\" ORDER BY \"ID\" asc");
+$result = pg_query($db_from, "SELECT * FROM public.\"Person\" ORDER BY \"ID\" asc");
 if (!$result) {
     echo "An error occurred.\n";
     exit;
 }
 
-$arr = pg_fetch_all($result);
-echo "Results: " . count($arr);
-echo "<table border='1'>";
-$json = array();
-foreach ($arr as $mar) {
-	$resa = array();
-	foreach ($mar as $k=>$v) {
-		//array_push($resa,"\"$k\": \"$v\"");
-		array_push($resa, "$v");
-	}
-	
-	
-	array_push($json, "<tr><td>" . implode("</td><td>", $resa) . "</td></tr>");
+$gender = array( "M" => "Male", "F" => "Female");
 
+$i = 0;
+
+// Loop over each person in the list
+while ($row = pg_fetch_array($result)) {
+    // do stuff with row
+    if ($i++ == 10) return;
+        $arr = array ("BYUID" => $row['ID'],
+                        "BirthDate" => $row['BirthDate'],
+                        "BirthPlace" => $row["BirthPlace"],
+                        "DeathDate" => $row['DeathDate'],
+                        "DeathPlace" => $row["DeathPlace"],
+                        "BurialPlace" => $row["BurialPlace"],
+                        "Gender" => $gender[$row["Gender"]]);
+    if ($row["LDS"] == 1)
+            $arr["LDSMember"] = "true";
+    else
+            $arr["LDSMember"] = "false";
+    $insert = get_insert_statement("Person", $arr);
+    $res = pg_query($db_to, $insert);
+    if ($res) {
+        $res = pg_query($db_to, "SELECT lastval()");
+        $temprow = pg_fetch_Array($res);
+        $id = $temprow[0];
+
+        $arr = array ("PersonID" => $id,
+            "First" => $row['GivenName'],
+            "Last" => $row['Surname'],
+            "Type" => "authoritative");
+        $insert = get_insert_statement("Name", $arr);
+        $res = pg_query($db_to, $insert);
+        return;
+
+    }
+    echo "==================================================\nUpdating record for {$row["ID"]}.\n";
+    //echo $insert . "\n";
+    $key = 1; // insert statement goes here, hopefully returning the id
+
+
+    
 
 }
-	echo implode("", $json);
 
-echo "</table>";
+function get_insert_statement($tableName, $arr) {
+    $insert = "INSERT INTO public.\"$tableName\" ";
+    $cols = "";
+    $vals = "";
+    foreach ($arr as $k => $v) {
+            $cols .= "\"$k\",";
+            if ($k == "BYUID")
+                $vals .= "$v,";
+            else
+                $vals .= "'$v',";
+    }
+    $cols = substr($cols, 0, -1);
+    $vals = substr($vals, 0, -1);
 
+    $insert .= "($cols) VALUES ($vals);";
+
+    return $insert;
+}
 ?>
