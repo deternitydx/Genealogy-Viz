c671d52ff5f9660edd75647997ca226e1de594a7
Robbie Hott <jh2jf@virginia.edu>
2014-07-10 17:51:12 -0400

    Robust and ready person importer from BYU to UVA

--- prototype/data_view/conversion/person.php
+++ prototype/data_view/conversion/person.php
@@ -16,45 +16,71 @@ $gender = array( "M" => "Male", "F" => "Female");
 
 $i = 0;
 
+$places = array();
+
+// grab all the places
+$places = load_places($db_to);
+
+print_r($places);
 // Loop over each person in the list
 while ($row = pg_fetch_array($result)) {
     // do stuff with row
-    if ($i++ == 10) return;
+        if ($i++ == 10) return;
+        $birthplace = "";
+        $deathplace = "";
+        $burialplace = "";
+
+        if ($row["ChildOfMarriageID"] == "") 
+                $row["ChildOfMarriageID"] = "NULL";
+
+        if (array_key_exists($row["BirthPlace"], $places))
+                $birthplace = $places[$row["BirthPlace"]];
+        else
+                $birthplace = get_place($row["BirthPlace"]);
+
+        if (array_key_exists($row["DeathPlace"], $places))
+                $deathplace = $places[$row["DeathPlace"]];
+        else
+                $deathplace = get_place($row["DeathPlace"]);
+
+        if (array_key_exists($row["BurialPlace"], $places))
+                $burialplace = $places[$row["BurialPlace"]];
+        else
+                $burialplace = get_place($row["BurialPlace"]);
+
         $arr = array ("BYUID" => $row['ID'],
                         "BirthDate" => $row['BirthDate'],
-                        "BirthPlace" => $row["BirthPlace"],
+                        "BirthPlaceID" => $birthplace,
                         "DeathDate" => $row['DeathDate'],
-                        "DeathPlace" => $row["DeathPlace"],
-                        "BurialPlace" => $row["BurialPlace"],
-                        "Gender" => $gender[$row["Gender"]]);
-    if ($row["LDS"] == 1)
-            $arr["LDSMember"] = "true";
-    else
-            $arr["LDSMember"] = "false";
+                        "DeathPlaceID" => $deathplace,
+                        "BurialPlaceID" => $burialplace,
+                        "Gender" => $gender[$row["Gender"]],
+                        "BYUChildOf" => $row['ChildOfMarriageID']);
+    $id = "";
+
     $insert = get_insert_statement("Person", $arr);
     $res = pg_query($db_to, $insert);
     if ($res) {
         $res = pg_query($db_to, "SELECT lastval()");
         $temprow = pg_fetch_Array($res);
+
+        // grab this person's ID in the new database
         $id = $temprow[0];
 
+        // insert the person's name as an authoritative name
         $arr = array ("PersonID" => $id,
             "First" => $row['GivenName'],
             "Last" => $row['Surname'],
             "Type" => "authoritative");
         $insert = get_insert_statement("Name", $arr);
         $res = pg_query($db_to, $insert);
-        return;
 
     }
-    echo "==================================================\nUpdating record for {$row["ID"]}.\n";
-    //echo $insert . "\n";
-    $key = 1; // insert statement goes here, hopefully returning the id
-
-
-    
-
+    echo "Importing record for {$row["ID"]} :: $id.\n";
 }
+
+print_r($places);
+
 
 function get_insert_statement($tableName, $arr) {
     $insert = "INSERT INTO public.\"$tableName\" ";
@@ -62,7 +88,7 @@ function get_insert_statement($tableName, $arr) {
     $vals = "";
     foreach ($arr as $k => $v) {
             $cols .= "\"$k\",";
-            if ($k == "BYUID")
+            if ($k == "BYUID" || $k == "BYUChildOf" || $v == "NULL")
                 $vals .= "$v,";
             else
                 $vals .= "'$v',";
@@ -73,5 +99,47 @@ function get_insert_statement($tableName, $arr) {
     $insert .= "($cols) VALUES ($vals);";
 
     return $insert;
+}
+
+function get_place($place) {
+    global $places;
+    global $db_to;
+    if (!isset($place) || empty($place) || $place == null || $place == "NULL" || $place == "" || $place == "null")
+            return "NULL";
+
+    echo "inserting for place: $place\n";
+    $arr = array("OfficialName" => $place);
+
+    $insert = get_insert_statement("Place", $arr);
+    $res = pg_query($db_to, $insert);
+
+    if ($res) {
+        $res = pg_query($db_to, "SELECT lastval()");
+        $temprow = pg_fetch_Array($res);
+        $id = $temprow[0];
+        $places[$place] = $id;
+        echo "inserted new place at: $id";
+        print_r($places);
+        return $id;
+    }
+
+    return "NULL";
+}
+
+function load_places($db) {
+    $places = array();
+
+    $result = pg_query($db, "SELECT * FROM public.\"Place\" ORDER BY \"ID\" asc");
+    if (!$result) {
+        echo "An error occurred.\n";
+        exit;
+    }
+
+    // Loop over each place in the list
+    while ($row = pg_fetch_array($result)) {
+        $places[$row["OfficialName"]] = $row["ID"];
+    }
+
+    return $places;
 }
 ?>
