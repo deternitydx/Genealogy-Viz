c6734ef9286eb37a4bdf9da35cee61d272be439c
Robbie Hott <jh2jf@virginia.edu>
2014-08-03 22:42:34 -0400

    Updated names script to grab correct ids and correctly update

--- prototype/data_view/conversion/import_names.php
+++ prototype/data_view/conversion/import_names.php
@@ -2,17 +2,19 @@
 
 $db_to = pg_connect("host=nauvoo.iath.virginia.edu dbname=nauvoo_data user=nauvoo password=p7qNpqygYU");
 
-$csvfile = fopen("aqdata.csv", "r");
+$csvfile = fopen("names.csv", "r");
 if ($csvfile == NULL)
         die("Error reading file");
 $head = fgetcsv($csvfile);
 $data1 = fgetcsv($csvfile);
+$lu = array("Alternate" => "alternate", "Authoritative" => "authoritative");
+$id = null;
 while ($data1 !== false) {
        $data = array();
        foreach ($data1 as $k => $v) 
-               $data[$head[$k]] = $v;
-       //print_r($data);
+               $data[$head[$k]] = trim($v);
        if ($data["ID"] != '') { // add man to AQ
+            echo "Looking up person {$data["ID"]}\n";
             $result = pg_query($db_to, "SELECT * FROM public.\"Person\" WHERE \"BYUID\"={$data["ID"]} ORDER BY \"ID\" asc");
             if (!$result) {
                 echo "An error occurred.\n";
@@ -20,13 +22,18 @@ while ($data1 !== false) {
             }
 
             $row = pg_fetch_array($result);
-            //print_r($row);
             $id = $row["ID"];
-            echo "Inserting ID $id into AQ\n";
-
-            $insert = get_insert_statement("ChurchOrgMembership", array("PersonID"=>$id, "ChurchOrgID"=>1));
-            $res = pg_query($db_to, $insert);
+            echo "Updating names of ID $id\n";
        }
+
+
+       $values = array("PersonID"=>$id, "First"=>$data["First Name"], "Middle"=>$data["Middle Name"], "Last"=>$data["Surname/Married  Name"],
+               "Prefix" =>$data["Prefix"], "Suffix"=>$data["Suffix"], "Type"=>$lu[$data["Type"]]);
+       $insert = get_insert_statement("Name", $values);
+       $update = get_update_statement("Name", $values, "\"PersonID\"=$id AND \"Type\"='{$lu[$data["Type"]]}'");
+       $query = "WITH upsert AS ($update RETURNING *) $insert WHERE NOT EXISTS (SELECT * FROM upsert);";
+       $res = pg_query($db_to, $insert);
+       echo $query . "\n";     
        $data1 = fgetcsv($csvfile);
 }
 
@@ -48,6 +55,28 @@ function get_insert_statement($tableName, $arr) {
     $vals = substr($vals, 0, -1);
 
     $insert .= "($cols) VALUES ($vals);";
+
+    return $insert;
+}
+
+function get_update_statement($tableName, $arr, $match) {
+    $insert = "UPDATE public.\"$tableName\" ";
+    $cols = "";
+    $vals = "";
+    foreach ($arr as $k => $v) {
+            $cols .= "\"$k\",";
+            if ($v == "") $v = "NULL";
+            if ($k == "BYUID" || $k == "BYUChildOf" || $v == "NULL")
+                $vals .= "$v,";
+            else
+                $vals .= "'$v',";
+    }
+    $cols = substr($cols, 0, -1);
+    $vals = substr($vals, 0, -1);
+
+    $insert .= " SET ($cols) = ($vals)";
+
+    $insert .= " WHERE $match;";
 
     return $insert;
 }
