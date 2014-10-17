2ae8fa9ec1af8a062406775392f31204a4257a7b
Robbie Hott <jh2jf@virginia.edu>
2014-10-17 16:19:05 -0400

    Data view tables now link parents and children

--- prototype/api/marriages.php
+++ prototype/api/marriages.php
@@ -4,20 +4,20 @@ header('Content-type: application/json');
 
 $id = 50;
 if (isset($_GET["id"]))
-	$id = $_GET["id"];
-	
+    $id = $_GET["id"];
+
 
 $db = pg_connect("host=nauvoo.iath.virginia.edu dbname=nauvoo_data user=nauvoo password=p7qNpqygYU");
 
 $result = pg_query($db, "SELECT DISTINCT m.\"ID\", m.\"MarriageDate\", m.\"DivorceDate\",m.\"CancelledDate\", m.\"Type\", 
-       h.\"PersonID\" as \"HusbandID\", 
-       hn.\"Last\" as \"HusbandLast\", hn.\"First\" as \"HusbandFirst\", w.\"PersonID\" as \"WifeID\", wn.\"Last\" as \"WifeLast\", wn.\"First\" as \"WifeFirst\"
-       FROM public.\"Marriage\" m, public.\"PersonMarriage\" h, public.\"PersonMarriage\" w, public.\"Name\" wn, public.\"Name\" hn
-       WHERE
-       h.\"MarriageID\" = m.\"ID\" AND h.\"Role\" = 'Husband' AND w.\"MarriageID\" = m.\"ID\" AND w.\"Role\" = 'Wife' 
-       AND wn.\"PersonID\" = w.\"PersonID\" AND wn.\"Type\" = 'authoritative'
-       AND hn.\"PersonID\" = h.\"PersonID\" AND hn.\"Type\" = 'authoritative'
-       ORDER BY m.\"ID\" DESC");
+    h.\"PersonID\" as \"HusbandID\", 
+    hn.\"Last\" as \"HusbandLast\", hn.\"First\" as \"HusbandFirst\", w.\"PersonID\" as \"WifeID\", wn.\"Last\" as \"WifeLast\", wn.\"First\" as \"WifeFirst\"
+    FROM public.\"Marriage\" m, public.\"PersonMarriage\" h, public.\"PersonMarriage\" w, public.\"Name\" wn, public.\"Name\" hn
+    WHERE
+    h.\"MarriageID\" = m.\"ID\" AND h.\"Role\" = 'Husband' AND w.\"MarriageID\" = m.\"ID\" AND w.\"Role\" = 'Wife' 
+    AND wn.\"PersonID\" = w.\"PersonID\" AND wn.\"Type\" = 'authoritative'
+    AND hn.\"PersonID\" = h.\"PersonID\" AND hn.\"Type\" = 'authoritative'
+    ORDER BY m.\"ID\" DESC");
 if (!$result) {
     echo "1An error occurred.\n";
     exit;
@@ -28,22 +28,25 @@ echo "{ \n\"data\": [\n";
 $json = array();
 $first = true;
 foreach ($arr as $mar) {
-        $resa = array();
-        $firsta = array();
+    $resa = array();
+    $firsta = array();
     foreach ($mar as $k=>$v) {
-            $clean = htmlspecialchars($v);
-            if ($clean == "") $clean = "&nbsp;";
+        $clean = htmlspecialchars($v);
+        if ($clean == "") $clean = "&nbsp;";
         array_push($resa, "\"$clean\"");
         if ($first) array_push($firsta, "\"$k\"");
-	}
-	
-	
-	array_push($json, "[" . implode(", ", $resa) . "]");
-	$first = false;
+    }
+
+    // add a link to children at the end
+    array_push($resa, "\"<a href='/nauvoo/data_view/people.php?parentSearch={$mar["ID"]}'>View</a>\"");
+    if ($first) array_push($firsta, "\"Children\"");
+
+    array_push($json, "[" . implode(", ", $resa) . "]");
+    $first = false;
 
 
 }
-	echo implode(",\n", $json);
+echo implode(",\n", $json);
 
 echo "]\n }";
 
--- prototype/api/people.php
+++ prototype/api/people.php
@@ -5,7 +5,8 @@ header('Content-type: application/json');
 $db = pg_connect("host=nauvoo.iath.virginia.edu dbname=nauvoo_data user=nauvoo password=p7qNpqygYU");
 
 $result = pg_query($db, "SELECT p.\"ID\",n.\"First\",n.\"Middle\",n.\"Last\",p.\"BirthDate\",p.\"DeathDate\",
-       p.\"Gender\", p.\"BirthPlaceID\" FROM public.\"Person\" p, public.\"Name\" n WHERE p.\"ID\"=n.\"PersonID\" AND n.\"Type\"='authoritative'ORDER BY n.\"Last\", n.\"First\",n.\"Middle\" asc");
+    p.\"Gender\", p.\"BirthPlaceID\", p.\"BiologicalChildOfMarriage\" as \"ChildOf\" FROM public.\"Person\" p, public.\"Name\" n
+    WHERE p.\"ID\"=n.\"PersonID\" AND n.\"Type\"='authoritative'ORDER BY n.\"Last\", n.\"First\",n.\"Middle\" asc");
 if (!$result) {
     exit;
 }
@@ -15,22 +16,25 @@ echo "{ \n\"data\": [\n";
 $json = array();
 $first = true;
 foreach ($arr as $mar) {
-        $resa = array();
-        $firsta = array();
+    $resa = array();
+    $firsta = array();
     foreach ($mar as $k=>$v) {
-            $clean = htmlspecialchars($v);
-            if ($clean == "") $clean = "&nbsp;";
+        $clean = htmlspecialchars($v);
+        if ($clean == "") $clean = "&nbsp;";
+        if ($k=="ChildOf") {
+            $clean = "<a href='/nauvoo/data_view/marriages.php?idSearch=$clean'>$clean</a>";    
+        }
         array_push($resa, "\"$clean\"");
         if ($first) array_push($firsta, "\"$k\"");
-	}
-	
-	
-	array_push($json, "[" . implode(", ", $resa) . "]");
-	$first = false;
+    }
+
+
+    array_push($json, "[" . implode(", ", $resa) . "]");
+    $first = false;
 
 
 }
-	echo implode(",\n", $json);
+echo implode(",\n", $json);
 
 echo "]\n }";
 
