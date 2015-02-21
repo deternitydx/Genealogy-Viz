a0c301dd292b92cc020bdb459cc4f972e1457813
Robbie Hott <jh2jf@virginia.edu>
2015-02-21 17:43:25 -0500

    Added more info to the api
    More of the person's information is added to the edit api.

--- prototype/api/edit_person.php
+++ prototype/api/edit_person.php
@@ -12,24 +12,64 @@
     
     // Array to hold all information about the person
     $person = array();
-    
+
+    // Get Personal Information
     $result = pg_query($db, "SELECT * FROM public.\"Person\" p
     WHERE p.\"ID\"=$id");
     if (!$result) {
         exit;
     }
-    foreach($result as $res) {
+    foreach(pg_fetch_all($result) as $res) {
         $person["information"] = $res;
     }
-    
-    $result = pg_query($db, "SELECT * FROM public.\"Name\" n WHERE n.\"ID\"=$id");
+
+    // Get All Names
+    $result = pg_query($db, "SELECT * FROM public.\"Name\" n WHERE n.\"PersonID\"=$id");
     if (!$result) {
         exit;
     }
-    foreach($result as $res) {
-        $person["names"] = $res;
+    $person["names"] = pg_fetch_all($result);
+
+    // Get Non-Marital Sealings
+    $result = pg_query($db, "SELECT * FROM public.\"NonMaritalSealings\" n WHERE n.\"AdopteeID\"=$id");
+    if (!$result) {
+        exit;
     }
+    $person["non_marital_sealings"] = pg_fetch_all($result);
+
+    // Get Temple Rites
+    $result = pg_query($db, "SELECT * FROM public.\"NonMaritalTempleRites\" n WHERE n.\"PersonID\"=$id");
+    if (!$result) {
+        exit;
+    }
+    $person["temple_rites"] = pg_fetch_all($result);
+
+
+    // Get All Marriages
+    $result = null;
+    if ($person["information"]["Gender"] == "Male") 
+        $result = pg_query($db, "SELECT m.\"ID\", m.\"PlaceID\", m.\"MarriageDate\", m.\"DivorceDate\",
+                                    m.\"CancelledDate\", m.\"Type\", w.\"PersonID\" as \"WifeID\", 
+                                    h.\"PersonID\" as \"HusbandID\", m.\"Root\" FROM public.\"Marriage\" m, 
+                                    public.\"PersonMarriage\" h, public.\"PersonMarriage\" w WHERE
+                                    h.\"MarriageID\" = m.\"ID\" AND h.\"Role\" = 'Husband' 
+                                    AND w.\"MarriageID\" = m.\"ID\" AND w.\"Role\" = 'Wife' 
+                                    AND h.\"PersonID\"=$id ORDER BY m.\"MarriageDate\" ASC");
+    else
+        $result = pg_query($db, "SELECT m.\"ID\", m.\"PlaceID\", m.\"MarriageDate\", m.\"DivorceDate\",
+                                    m.\"CancelledDate\", m.\"Type\", w.\"PersonID\" as \"WifeID\", 
+                                    h.\"PersonID\" as \"HusbandID\", m.\"Root\" FROM public.\"Marriage\" m, 
+                                    public.\"PersonMarriage\" h, public.\"PersonMarriage\" w WHERE
+                                    h.\"MarriageID\" = m.\"ID\" AND h.\"Role\" = 'Husband' 
+                                    AND w.\"MarriageID\" = m.\"ID\" AND w.\"Role\" = 'Wife' 
+                                    AND w.\"PersonID\"=$id ORDER BY m.\"MarriageDate\" ASC");
     
+    if (!$result) {
+        print_empty("Error finding marriages.");
+        exit;
+    }
+
+    $person["marriages"] = pg_fetch_all($result);
     // Return the person array as json to be used by the editor:
     echo json_encode($person);
 ?>
