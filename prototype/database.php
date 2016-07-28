3d4ce2bf496e632c4f23e2e025b08e2081fbcee5
Robbie Hott <jh2jf@virginia.edu>
2016-07-28 10:33:49 -0400

    Pointing to the original database for some reason

--- prototype/database.php
+++ prototype/database.php
@@ -1,5 +1,5 @@
 <?php
 
-$db_conn_string = "host=nauvoo.iath.virginia.edu dbname=nauvoo_data_test user=nauvoo password=p7qNpqygYU";
+$db_conn_string = "host=nauvoo.iath.virginia.edu dbname=nauvoo_data user=nauvoo password=p7qNpqygYU";
 
 ?>
